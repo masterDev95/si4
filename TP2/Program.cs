694d342c6753023c6d839ca62412400ca60831c9
Alexis Ottina <masdevelopper@gmail.com>
2018-09-18 22:13:05 +0200

    TP2 v0.1
    Exercice 1 fini

--- TP2/Program.cs
+++ TP2/Program.cs
@@ -10,6 +10,20 @@ namespace TP2
     {
         static void Main(string[] args)
         {
+            //--
+        }
+
+        private static void petiteValeur()
+        {
+            decimal val1, val2;
+
+            Console.WriteLine("Veuillez saisir un nombre:");
+            val1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Veuillez saisir un deuxième nombre:");
+            val2 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("\n\nLe nombre le plus petit est {0}.", Math.Min(val1, val2));
         }
     }
 }
