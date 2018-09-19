e4b15007429e6d17df50693e75bddaa025aefdd0
Alexis Ottina <masdevelopper@gmail.com>
2018-09-19 21:22:30 +0200

    TP2 v0.2
    Exercice 2 fini

--- TP2/Program.cs
+++ TP2/Program.cs
@@ -10,7 +10,46 @@ namespace TP2
     {
         static void Main(string[] args)
         {
-            //--
+            int choix;
+
+            Console.WriteLine("\t-La plus petite valeur entre deux nombre(1)");
+            Console.WriteLine("\t-Calcul de prime sur le chiffre d'affaire(2)");
+
+            Console.WriteLine("Veuillez choisir la fonction que vous voulez (1-2)");
+            choix = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            switch (choix)
+            {
+                case 1:
+                    petiteValeur();
+                    break;
+                case 2:
+                    calculPrimeCA();
+                    break;
+                default:
+                    Console.WriteLine("Désolé, je n'ai pas compris votre requête..");
+                    break;
+            }
+        }
+
+        private static void calculPrimeCA()
+        {
+            decimal chiffreAffaire, montantPrime;
+            const decimal prime = 4, minChiffreAffaire = 1000;
+
+            Console.WriteLine("Quel est votre chiffre d'affaire réalisé ?");
+            chiffreAffaire = Convert.ToDecimal(Console.ReadLine());
+
+            if (chiffreAffaire >= minChiffreAffaire)
+            {
+                montantPrime = (prime / 100) * chiffreAffaire;
+                Console.WriteLine("\nLe montant de la prime de votre chiffre d'affaire s'élève à {0} euros", montantPrime);
+            }
+            else
+            {
+                Console.WriteLine("\nLe montant de votre chiffre d'affaire n'est pas assez élevé pour obtenir une prime.");
+            }
         }
 
         private static void petiteValeur()
