6527d63858a338132ee54255dcba940e7dbca580
aott <aott@W7-251-09>
2018-09-28 17:14:43 +0200

    TP4 v0.4 Exercice 4 fini

--- TP4/Program.cs
+++ TP4/Program.cs
@@ -10,7 +10,51 @@ namespace TP4
     {
         static void Main(string[] args)
         {
+            Exercice4();
+        }
 
+        private static void Exercice4()
+        {
+            List<int> tableau = new List<int>();
+            string saisie;
+
+            Console.WriteLine("Veuillez saisir dix valeurs numériques :");
+
+            for (int i = 0; i < 10; i++)
+            {
+                saisie = Console.ReadLine();
+                tableau.Add(Convert.ToInt32(saisie));
+            }
+
+            Console.WriteLine("\nVoici ce que vous avez saisie (que les paires):");
+
+            foreach (int valeur in tableau)
+            {
+                if (valeur % 2 == 0) Console.WriteLine(valeur);
+            }
+        }
+
+        private static void Exercice3()
+        {
+            List<int> tableau = new List<int>();
+            string saisie;
+            bool affiche = true;
+
+            Console.WriteLine("Veuillez saisir dix valeurs numériques :");
+
+            for (int i = 0; i < 10; i++)
+            {
+                saisie = Console.ReadLine();
+                tableau.Add(Convert.ToInt32(saisie));
+            }
+
+            Console.WriteLine("\nVoici ce que vous avez saisie (1 valeur sur 2):");
+
+            foreach (int valeur in tableau)
+            {
+                if (affiche) Console.WriteLine(valeur);
+                affiche = !affiche;
+            }
         }
 
         private static void Exercice2()
