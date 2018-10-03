ef66a8fa92249434860e2e297f92b34781afe6f6
Alexis Ottina <masdevelopper@gmail.com>
2018-10-03 19:03:35 +0200

    TP4 v1.0 - fini !

--- TP4/Program.cs
+++ TP4/Program.cs
@@ -10,33 +10,8 @@ namespace TP4
     {
         static void Main(string[] args)
         {
-            Exercice4();
-        }
-
-        private static void Exercice4()
-        {
             List<int> tableau = new List<int>();
-            string saisie;
-
-            Console.WriteLine("Veuillez saisir dix valeurs numériques :");
-
-            for (int i = 0; i < 10; i++)
-            {
-                saisie = Console.ReadLine();
-                tableau.Add(Convert.ToInt32(saisie));
-            }
-
-            Console.WriteLine("\nVoici ce que vous avez saisie (que les paires):");
-
-            foreach (int valeur in tableau)
-            {
-                if (valeur % 2 == 0) Console.WriteLine(valeur);
-            }
-        }
-
-        private static void Exercice3()
-        {
-            List<int> tableau = new List<int>();
+            List<int> aEnlever = new List<int>();
             string saisie;
             bool affiche = true;
 
@@ -48,47 +23,76 @@ namespace TP4
                 tableau.Add(Convert.ToInt32(saisie));
             }
 
-            Console.WriteLine("\nVoici ce que vous avez saisie (1 valeur sur 2):");
+            //2
+            Console.WriteLine("\nVoici ce que vous avez saisi:");
+
+            foreach (int valeur in tableau)
+            {
+                Console.WriteLine(valeur);
+            }
+
+            //3
+            Console.WriteLine("\nVoici ce que vous avez saisi (1 valeur sur 2):");
 
             foreach (int valeur in tableau)
             {
                 if (affiche) Console.WriteLine(valeur);
                 affiche = !affiche;
             }
-        }
 
-        private static void Exercice2()
-        {
-            List<int> tableau = new List<int>();
-            string saisie;
-
-            Console.WriteLine("Veuillez saisir dix valeurs numériques :");
-
-            for (int i = 0; i < 10; i++)
-            {
-                saisie = Console.ReadLine();
-                tableau.Add(Convert.ToInt32(saisie));
-            }
-
-            Console.WriteLine("\nVoici ce que vous avez saisie:");
+            //4
+            Console.WriteLine("\nVoici les paires de ce que vous avez saisi:");
 
             foreach (int valeur in tableau)
             {
-                Console.WriteLine(valeur);
+                if (valeur % 2 == 0) Console.WriteLine(valeur);
             }
-        }
 
-        private static void Exercice1()
-        {
-            List<int> tableau = new List<int>();
-            string saisie;
+            //5
+            Console.WriteLine("\nVoici la valeur maximale de ce que vous avez saisi:");
+            Console.WriteLine(tableau.Max());
 
-            Console.WriteLine("Veuillez saisir dix valeurs numériques :");
+            //6
+            Console.WriteLine("\nVoici la valeur minimale de ce que vous avez saisi:");
+            Console.WriteLine(tableau.Min());
 
-            for (int i = 0; i < 10; i++)
+            //7
+            Console.WriteLine("\nVoici la somme des valeurs que vous avez saisi:");
+            Console.WriteLine(tableau.Sum());
+
+            //8
+            Console.WriteLine("\nVoici la moyenne des valeurs que vous avez saisi:");
+            Console.WriteLine(tableau.Average());
+
+            //9
+            Console.WriteLine("\nVoici les nombres premiers que vous avez saisi:");
+
+            //SUPPRESSION VALEURES NON PREMIERES
+            foreach (int valeur in tableau)
             {
-                saisie = Console.ReadLine();
-                tableau.Add(Convert.ToInt32(saisie));
+                if (valeur % 2 == 0 && valeur != 2)
+                {
+                    aEnlever.Add(valeur);
+                }
+                else if (valeur % 3 == 0 && valeur != 3)
+                {
+                    aEnlever.Add(valeur);
+                }
+                else if (valeur % 7 == 0 && valeur != 7)
+                {
+                    aEnlever.Add(valeur);
+                }
+            }
+
+            foreach (int valeur in aEnlever)
+            {
+                tableau.Remove(valeur);
+            }
+
+            //AFFICHAGE
+            foreach (int valeur in tableau)
+            {
+                Console.WriteLine(valeur);
             }
         }
     }
