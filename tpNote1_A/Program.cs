f776118edae340b697b2e6b035aea95c53baeeb1
aott <aott@W7-251-10>
2018-10-05 15:08:08 +0200

    tp1.A noté - Fin

--- tpNote1_A/Program.cs
+++ tpNote1_A/Program.cs
@@ -10,7 +10,74 @@ namespace tpNote1_A
     {
         static void Main(string[] args)
         {
-            decimal coutTrajet, coutHebergement,coutNourriture = 3.5m;
+            int choix;
+
+            Console.WriteLine("\t-Exercice 2 (exercice sur le voyage au Futuroscope)(1)");
+            Console.WriteLine("\t-Exercice 3 (exercice sur le voyage scolaire)(2)");
+
+            Console.WriteLine("Veuillez choisir la fonction que vous voulez (1-2)");
+            choix = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            switch (choix)
+            {
+                case 1:
+                    Exercice2();
+                    break;
+                case 2:
+                    Exercice3();
+                    break;
+                default:
+                    Console.WriteLine("Désolé, je n'ai pas compris votre requête..");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Exercice3()
+        {
+            decimal coutTrajet, coutHebergement, coutNourriture = 3.5m;
+            decimal coutParEleve, coutTotal;
+            int nbEleve, nbJour;
+
+            Console.WriteLine("Combien d'élève vont participer au voyage?");
+            nbEleve = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nPendant combien de jours?");
+            nbJour = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\n\n");
+
+            if (nbEleve > 25)
+            {
+                coutTrajet = 61 * nbEleve;
+            }
+            else
+            {
+                coutTrajet = 67.30m * nbEleve;
+            }
+
+            if (nbEleve > 35)
+            {
+                coutHebergement = 3.5m * nbEleve;
+            }
+            else
+            {
+                coutHebergement = 4.75m * nbEleve;
+            }
+
+            coutNourriture = coutNourriture * nbJour * nbEleve;
+            coutHebergement *= nbEleve;
+            coutTotal = coutTrajet + coutHebergement + coutNourriture;
+
+            //COUT PAR ELEVE
+            coutParEleve = coutTrajet / nbEleve;
+            coutParEleve += coutHebergement / nbEleve;
+            coutParEleve += coutNourriture / nbEleve;
+
+            Console.WriteLine("\t-Prix de revient par élève: {0} euros", coutParEleve);
+            Console.WriteLine("\t-Cout global ({0} élève): {1} euros", nbEleve, coutTotal);
         }
 
         private static void Exercice2()
