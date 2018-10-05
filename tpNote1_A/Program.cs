5f0863e521d2bf8000dc33a5a98b1bbf0d0c5ceb
aott <aott@W7-253-01>
2018-10-05 13:46:29 +0200

    tpNote1_A v0.1

--- tpNote1_A/Program.cs
+++ tpNote1_A/Program.cs
@@ -10,7 +10,40 @@ namespace tpNote1_A
     {
         static void Main(string[] args)
         {
+            decimal coutTrajet, coutHebergement,coutNourriture = 3.5m;
+        }
 
+        private static void Exercice2()
+        {
+            int nbEnfant, reduction;
+
+            Console.WriteLine("Combien d'enfant(s) doit embarquer pour aller au Futuroscope?");
+            nbEnfant = Convert.ToInt32(Console.ReadLine());
+
+            //CALCUL REDUCTION
+            switch (nbEnfant)
+            {
+                case 2:
+                    reduction = 10;
+                    break;
+                case 3:
+                    reduction = 15;
+                    break;
+                case 4:
+                    reduction = 18;
+                    break;
+                default:
+                    reduction = 0;
+                    break;
+            }
+
+            if (nbEnfant > 4)
+            {
+                reduction = 18;
+                reduction += nbEnfant - 4;
+            }
+
+            Console.WriteLine("Vous avez le droit a une réduction de {0}%", reduction);
         }
     }
 }
