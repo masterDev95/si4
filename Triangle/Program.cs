29b84faed32aa0ac4bfa768e0d851ca984a4a34e
aott <aott@W7-253-01>
2018-10-12 15:40:27 +0200

    Traingle pas fini

---
+++ Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hauteur, caraTotal, caraLigne;
+
+            Console.WriteLine("Hauteur?");
+            hauteur = Convert.ToInt32(Console.ReadLine());
+            caraLigne = hauteur + hauteur - 1;
+            caraTotal = caraLigne * hauteur;
+
+            Console.WriteLine("\n");
+
+            for (int i = 1; i <= caraTotal; i++)
+            {
+                bool eval = i < (hauteur - i);
+                if (eval)
+                {
+
+                }
+            }
+        }
+    }
+}
