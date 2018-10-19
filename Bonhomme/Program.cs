6bb8408fa0054f7470e99d4a4ba05d525a222c04
Alexis Ottina <masdevelopper@gmail.com>
2018-10-19 18:12:35 +0200

    Bonhomme v0.1

---
+++ Bonhomme/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonhomme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] bonhomme = new string[]
+            {
+                " ", " ", "*", " ", " ",
+                " ", "*", " ", "*", " ",
+                " ", "*", " ", "*", " ",
+                " ", " ", "*", " ", " ",
+                " ", " ", "*", " ", " ",
+                "*", "*", "*", "*", "*",
+                " ", " ", "*", " ", " ",
+                " ", " ", "*", " ", " ",
+                " ", " ", "*", " ", " ",
+                " ", "*", " ", "*", " ",
+                "*", " ", " ", " ", "*",
+            };
+            const int bonhomme_largeur = 5, bonhomme_longueur = 11;
+            int x = Console.WindowWidth / 2 - bonhomme_largeur / 2;
+            int y = (Console.WindowHeight / 2 - bonhomme_longueur / 2) - 1;
+
+            Console.SetCursorPosition(x, y);
+
+            for (int i = 0; i < bonhomme.Length; i++)
+            {
+                if (i % bonhomme_largeur == 0)
+                {
+                    Console.WriteLine("");
+                }
+
+                Console.Write(bonhomme[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
