93a6a2b4ca420f2f27a8c09ae41ceb176c1a1d27
Alexis Ottina <masdevelopper@gmail.com>
2018-10-19 19:40:27 +0200

    Bonhomme v1.1

--- Bonhomme/Program.cs
+++ Bonhomme/Program.cs
@@ -12,35 +12,83 @@ namespace Bonhomme
         {
             string[] bonhomme = new string[]
             {
-                " ", " ", "*", " ", " ",
-                " ", "*", " ", "*", " ",
-                " ", "*", " ", "*", " ",
-                " ", " ", "*", " ", " ",
-                " ", " ", "*", " ", " ",
-                "*", "*", "*", "*", "*",
-                " ", " ", "*", " ", " ",
-                " ", " ", "*", " ", " ",
-                " ", " ", "*", " ", " ",
-                " ", "*", " ", "*", " ",
-                "*", " ", " ", " ", "*",
+                " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "0", ")", " ", "\\", " ", " ", " ", " ",
+                " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "[", "]", "\\", "_", " ", "\\", " ", " ", " ",
+                " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "_", "_", "_", "[", "}", "_", "_", "/", "-", " ", " ", " ",
+                " ", " ", " ", " ", " ", " ", " ", " ", " ", "/", "-", "-", "-", "-", "\\", "\\", " ", "=", "/", "\\", "_", " ",
+                "O", "O", "0", "0", "o", "o", ".", ".", ".", "=", "/", "_", "\\", "_", "#", "|", "[", "_", "]", "/", "\\", "\\",
+                " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", "\\", "_", "/", " ", " ", " ", " ", " ", " ", "\\", "_", "/",
             };
-            const int bonhomme_largeur = 5, bonhomme_longueur = 11;
-            int x = Console.WindowWidth / 2 - bonhomme_largeur / 2;
-            int y = (Console.WindowHeight / 2 - bonhomme_longueur / 2) - 1;
 
-            Console.SetCursorPosition(x, y);
+            ConsoleKeyInfo toucheAppuye;
+            const int bonhomme_largeur = 22, bonhomme_longueur = 7;
+            int xx = Console.WindowWidth / 2 - bonhomme_largeur / 2;
+            int yy = Console.WindowHeight / 2 - bonhomme_longueur / 2;
+            int x = xx, y = yy;
 
-            for (int i = 0; i < bonhomme.Length; i++)
+            Console.CursorVisible = false;
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+
+            do
             {
-                if (i % bonhomme_largeur == 0)
+                for (int i = 0; i < bonhomme.Length; i++, x++)
                 {
-                    Console.WriteLine("");
+                    if (i % bonhomme_largeur == 0)
+                    {
+                        y++;
+                        x = xx;
+                    }
+
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(bonhomme[i]);
                 }
 
-                Console.Write(bonhomme[i]);
-            }
+                toucheAppuye = Console.ReadKey();
 
-            Console.ReadLine();
+                switch (toucheAppuye.Key)
+                {
+                    case ConsoleKey.Escape:
+                        Environment.Exit(500);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        xx--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        xx++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        yy--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        yy++;
+                        break;
+                }
+
+                //Limiter la position du bonhomme
+                if (xx < 0)
+                {
+                    xx = 0;
+                }
+                else if (xx > Console.WindowWidth - bonhomme_largeur)
+                {
+                    xx = Console.WindowWidth - bonhomme_largeur;
+                }
+
+                if (yy < -1)
+                {
+                    yy = -1;
+                }
+                else if (yy > Console.WindowHeight - bonhomme_longueur)
+                {
+                    yy = Console.WindowHeight - bonhomme_longueur;
+                }
+
+                //Mise à Jour Position
+                y = yy;
+
+                //Nettoyer l'écran
+                Console.Clear();
+            } while (true);
         }
     }
 }
