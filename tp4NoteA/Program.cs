3fc6ee24fcfb6dfad6ee600a050ea746af3d35e0
aott <aott@W7-253-03>
2018-11-12 11:53:20 +0100

    tp4NoteA - Fini

---
+++ tp4NoteA/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp4NoteA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercice2();
+        }
+
+        private static void Exercice2()
+        {
+            int[] tab1 = { 2, 7, 8, 9, 10 };
+            int[] tab2 = { 1, 2, 3, 4, 5 };
+            int calcul, somme = 0;
+
+            for (int i = 0; i < tab1.Length; i++)
+            {
+                int temp = tab2.Length - i - 1;
+                calcul = tab1[i] * tab2[temp];
+                somme += calcul;
+                Console.WriteLine("tab1[{0}] * tab2[{1}] = {2}", i, temp, calcul);
+            }
+
+            Console.WriteLine("La somme des produits croisés est égale à {0}", somme);
+            Console.ReadLine();
+        }
+
+        private static void Exercice1()
+        {
+            decimal saisieUtilisateur, resultat = 0, multiplication = 0;
+
+            Console.WriteLine("Veuillez saisir un nombre:");
+            saisieUtilisateur = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            for (int i = 0; i < saisieUtilisateur; i++)
+            {
+                if (i == 0)
+                {
+                    Console.Write("1 + 1/1 ");
+                    resultat += 2;
+                }
+                else
+                {
+                    Console.Write("+ 1/(");
+
+                    for (int j = 0; j <= i; j++)
+                    {
+                        if (j == 0)
+                        {
+                            Console.Write("{0}", j + 1);
+                            multiplication = 1;
+                        }
+                        else
+                        {
+                            Console.Write("*{0}", j + 1);
+                            multiplication *= j + 1;
+                        }
+                    }
+
+                    Console.Write(") ");
+
+                    resultat += 1 / multiplication;
+                }
+            }
+
+            Console.WriteLine("\nLe resultat est {0}", resultat);
+            Console.ReadLine();
+        }
+    }
+}
