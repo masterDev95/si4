52f89bbb9201b926a8470f0fedfcd7b42326dbd9
aott <aott@W7-253-01>
2018-11-16 15:37:47 +0100

    TP Collection - Init

---
+++ TP_Collection/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
