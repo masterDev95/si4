ff966e3e94bc25862d9ff78ad0893d1af493e899
aott <aott@W7-253-01>
2018-11-16 17:12:00 +0100

    TP Polybe - complété

---
+++ TP Polybe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_Polybe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] alphabet = {
+                "a","b","c","d","e",
+                "f","g","h","ij","k",
+                "l","m","n","o","p",
+                "q","r","s","t","u",
+                "v","w","x","y","z"
+            };
+            string word = "hello world";
+            string[] split = word.Split();
+
+            for ()
+        }
+    }
+}
